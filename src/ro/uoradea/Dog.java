81f7ee2ecac6839458b65e706ddb16e7cad7f1bb
Cernei Silvio-Ricardo <cti22a115@cst.uoradea.local>
2023-10-30 19:02:24 +0200

    Initial commit

---
+++ src/ro/uoradea/Dog.java
@@ -0,0 +1,10 @@
+package ro.uoradea;
+
+public class Dog {
+     private String name;
+     private int age;
+
+
+
+}
+
---
+++ src/ro/uoradea/Main.java
@@ -0,0 +1,9 @@
+package ro.uoradea;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Dog dog1 = new Dog();
+        Dog dog2 = new Dog();
+    }
+}
